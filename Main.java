7e857a44340337c5861de3d1510fede6067e7020
andadariangelina <120695657+andadariangelina@users.noreply.github.com>
2023-05-30 17:12:38 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -8,11 +8,11 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) throws IOException {
         try{
-            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 131),0);
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8131),0);
             httpserver.createContext("/", new Server());
             httpserver.setExecutor(Executors.newSingleThreadExecutor());
             httpserver.start();
-            System.out.println("Listening on port 131...");
+            System.out.println("Listening on port 8131...");
         }catch(Exception e){
             e.printStackTrace();
         }
