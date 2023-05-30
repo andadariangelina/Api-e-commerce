2487fbd7b39b798f47b60ef0807f60ed2440c137
andadariangelina <120695657+andadariangelina@users.noreply.github.com>
2023-05-30 17:09:11 +0800

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,129 @@
+package com.example;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.json.simple.JSONObject;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+
+import org.json.simple.JSONArray;
+
+public class Data {
+  public JSONObject selectDatabase(String[] path, String query){
+    try {
+      Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/indianajones/Documents/college/pbo/Simple-Java-API-for-E-Commerce/ecommerce.db");
+      Statement statement = connection.createStatement();
+      System.out.println("connection berhasil");
+      if(path[1].equals("users")){
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+        if(path.length == 2){
+          ResultSet rs = statement.executeQuery("select * from " + path[1]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("id", rs.getInt("id"));
+            record.put("First_Name", rs.getString("first_name"));
+            record.put("Last_Name", rs.getString("last_name"));
+            record.put("Email", rs.getString("email"));
+            record.put("Phone Number", rs.getString("phone_number"));
+            record.put("Type", rs.getString("type"));
+            array.add(record);
+          }
+          jsonObject.put("User Information", array);
+          return jsonObject;
+        }
+        if(path.length == 3){
+          ResultSet rs = statement.executeQuery("select * from " + path[1] + " where id=" + path[2]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("id", rs.getInt("id"));
+            record.put("First_Name", rs.getString("first_name"));
+            record.put("Last_Name", rs.getString("last_name"));
+            record.put("Email", rs.getString("email"));
+            record.put("Phone Number", rs.getString("phone_number"));
+            record.put("Type", rs.getString("type"));
+            array.add(record);
+          }
+          jsonObject.put("User Information", array);
+          return jsonObject;
+        }
+      }
+      else if(path[1].equals("products")){
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+        System.out.println(query);
+        if(path.length == 2){
+          ResultSet rs = statement.executeQuery("select * from " + path[1]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("Stock", rs.getInt("stock"));
+            record.put("Price", rs.getString("price"));
+            record.put("Description", rs.getString("description"));
+            record.put("Title", rs.getString("title"));
+            record.put("Id", rs.getInt("id_products"));
+            array.add(record);
+          }
+          jsonObject.put("Product Information", array);
+          return jsonObject;
+        }
+        if(path.length == 3){
+          ResultSet rs = statement.executeQuery("select * from " + path[1] + " where id_products=" + path[2]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("Stock", rs.getInt("stock"));
+            record.put("Price", rs.getString("price"));
+            record.put("Description", rs.getString("description"));
+            record.put("Title", rs.getString("title"));
+            record.put("Id", rs.getInt("id_products"));
+            array.add(record);
+          }
+          jsonObject.put("Product Information", array);
+          return jsonObject;
+        }
+      }
+      else if(path[1].equals("orders")){
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+        if(path.length == 2){
+          ResultSet rs = statement.executeQuery("select * from " + path[1]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("isPaid", rs.getInt("isPaid"));
+            record.put("Discount", rs.getInt("discount"));
+            record.put("Total", rs.getInt("total"));
+            record.put("Note", rs.getInt("note"));
+            record.put("Id", rs.getInt("id"));
+            array.add(record);
+          }
+          jsonObject.put("Orders Information", array);
+          return jsonObject;
+        }
+        if(path.length == 3){
+          ResultSet rs = statement.executeQuery("select * from " + path[1] + " where id=" + path[2]);
+          while(rs.next()) {
+            JSONObject record = new JSONObject();
+            record.put("isPaid", rs.getInt("isPaid"));
+            record.put("Discount", rs.getInt("discount"));
+            record.put("Total", rs.getInt("total"));
+            record.put("Note", rs.getInt("note"));
+            record.put("Id", rs.getInt("id"));
+            array.add(record);
+          }
+          jsonObject.put("Orders Information", array);
+          return jsonObject;
+        }
+      }
+      
+    } catch(SQLException e) {
+      System.err.println(e.getMessage());
+    }
+    return null;
+  }
+  // private JSONObject getData(JSONObject object, JSONArray array, ResultSet rs){
+
+  // }
+}
